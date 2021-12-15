481fb11cd4ed2dd15a5946154422a1e0d5493de5
Arvydas Apulskis <arvydas.apulskis@visma.com>
2021-12-15 22:07:15 +0200

    Add bo log parser test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,14 @@ namespace Tests;
 
 abstract class TestCase extends \PHPUnit\Framework\TestCase
 {
+    protected function getUnitFixturesDir(?string $path = null): string
+    {
+        $base = __DIR__ . '/Unit/Fixtures';
 
+        if ($path === null) {
+            return $base;
+        }
+
+        return "{$base}/{$path}";
+    }
 }
