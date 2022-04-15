b68bae8bb15472e84a462bfefab6d50b915cd916
Arvydas Apulskis <arvydas.apulskis@visma.com>
2022-04-15 14:16:48 +0300

    Improve data dog client and move cloudwatch to clients namespace

--- src/Client/DataDog/DataDogClient.php
+++ src/Client/DataDog/DataDogClient.php
@@ -6,32 +6,33 @@ namespace App\Client\DataDog;
 
 use App\Constant\Value\DataDogEndpoint;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Exception\RequestException;
 
 class DataDogClient
 {
-    private string $host = 'https://api.datadoghq.eu/api/v2'; // TODO configurable
-
-    public function __construct(private Client $client)
-    {
+    public function __construct(
+        private Client $client,
+        private string $host,
+        private string $appKey,
+        private string $apiKey,
+    ) {
     }
 
-    /**
-     * @throws \GuzzleHttp\Exception\GuzzleException
-     */
     public function getLogs(Filter $filter): iterable
     {
-        $logsCount = 0;
-
         $data = [
             'filter' => $filter,
             'page' => [
                 'cursor' => null,
-                'limit' => 100, // Max limit is 5000
+                // Max limit is 5000, but we use smaller chunks to preserve memory
+                'limit' => 100,
             ],
         ];
 
         do {
+            $startTime = microtime(true);
+
             $response = $this->request(DataDogEndpoint::LOG_SEARCH, 'POST', $data);
 
             if ($response === null) {
@@ -39,30 +40,34 @@ class DataDogClient
             }
 
             foreach ($response['data'] ?? [] as $log) {
-                $logsCount++;
-                echo "\rLogs: {$logsCount}";
-
                 yield $log;
             }
 
             $data['page']['cursor'] = $response['meta']['page']['after'] ?? null;
 
-            // Needed for rate limit
-            // TODO make dynamic, if program is running more than 3 seconds between requests skip sleep
-            // TODO: also if cursor is null, that means it is last request so it should skip sleep also
-            sleep(3);
-        } while ($data['page']['cursor'] !== null);
+            $endTime = microtime(true);
 
-        echo PHP_EOL;
+            $executionTime = (int) round($endTime - $startTime);
+
+            // Needed for rate limit
+            $minimumExecutionTime = 3;
+
+            if ($executionTime < $minimumExecutionTime && $data['page']['cursor'] !== null) {
+                sleep($minimumExecutionTime - $executionTime);
+            }
+        } while ($data['page']['cursor'] !== null);
     }
 
     protected function request(string $endpoint, string $method, array $data = []): ?array
     {
         try {
             $response = $this->client->request($method, $this->getUrl($endpoint), $this->getOptions($method, $data));
-        } catch (RequestException $exception) {
-            // TODO log this and continue working? return null;
-            die($exception->getMessage());
+        } catch (RequestException) {
+            // TODO log this exception to logger
+            return null;
+        } catch (GuzzleException $e) {
+            // This is critical exception so we must stop
+            die($e->getMessage());
         }
 
         return json_decode($response->getBody()->getContents(), true);
@@ -84,9 +89,8 @@ class DataDogClient
         if (in_array($method, ['POST', 'PUT', 'PATCH'])) {
             $options['json'] = $data;
             $options['headers'] = [
-                // TODO: somehow get the key, maybe when creating a client add these headers
-                'DD-API-KEY' => 'todo',
-                'DD-APPLICATION-KEY' => 'todo ',
+                'DD-API-KEY' => $this->apiKey,
+                'DD-APPLICATION-KEY' => $this->appKey,
             ];
         } else {
             $options['query'] = $data;
--- src/Client/DataDog/Filter.php
+++ src/Client/DataDog/Filter.php
@@ -13,6 +13,30 @@ class Filter implements \JsonSerializable
     ) {
     }
 
+    /**
+     * @throws \Exception
+     */
+    public static function fromJsonString(string $json): self
+    {
+        $json = json_decode($json);
+
+        if ($json === null || self::hasInvalidStructure($json)) {
+            throw new \Exception('Invalid json string provided');
+        }
+
+        return new self(
+            $json->query ?? $json->filter->query,
+            new \DateTimeImmutable('@' . round(($json->from ?? $json->filter->from) / 1000)),
+            new \DateTimeImmutable('@' . round(($json->to ?? $json->filter->to) / 1000)),
+        );
+    }
+
+    private static function hasInvalidStructure(\stdClass $json): bool
+    {
+        return !isset($json->query, $json->from, $json->to)
+            && !isset($json->filter->query, $json->filter->from, $json->filter->to);
+    }
+
     public function jsonSerialize(): array
     {
         return [
