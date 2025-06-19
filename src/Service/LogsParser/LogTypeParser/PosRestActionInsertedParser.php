03b9b40b4727fd53be7393d3de44af5c76b85cf9
Arvydas Apulskis <arvydas.apulskis@twoday.com>
2025-06-19 16:16:10 +0300

    Add pos rest atcion insert parser

---
+++ src/Service/LogsParser/LogTypeParser/PosRestActionInsertedParser.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service\LogsParser\LogTypeParser;
+
+use App\Service\LogsParser\ParsedLog;
+
+class PosRestActionInsertedParser implements LogTypeParserInterface
+{
+    public static function getId(): string
+    {
+        return 'pos_rest_action_inserted';
+    }
+
+    public function parse(array $event): ?ParsedLog
+    {
+        $masterUserId = $event['attributes']['attributes']['usr']['id'] ?? null;
+        $data = json_decode($event['attributes']['attributes']['info'] ?? '');
+
+        if ($data === null || $masterUserId === null) {
+            return null;
+        }
+
+        $id = $data->restId ?? null;
+        $resource = $data->resourceName ?? null;
+        $method = $this->resolveMethod((int) ($data->type ?? 0));
+
+        if ($id === null || $resource === null || $method === null) {
+            return null;
+        }
+
+        // TODO: url cannot be extracted from logs so we must find other way to get them, now hard-coding
+        $url = "https://pos-etail.wallmob.com/{$resource}";
+
+        return new ParsedLog(json_encode($data->parameters), $method, $url, $id, $masterUserId);
+    }
+
+    private function resolveMethod(int $typeId): ?string
+    {
+        return match ($typeId) {
+            0 => 'GET',
+            1 => 'POST',
+            2 => 'PUT',
+            3 => 'PATCH',
+            4 => 'DELETE',
+            default => null,
+        };
+    }
+}
