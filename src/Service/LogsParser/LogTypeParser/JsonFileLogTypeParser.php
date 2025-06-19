562b3a9916980d2f9c2e05c04a4f88153bb8cd08
Arvydas Apulskis <arvydas.apulskis@twoday.com>
2025-06-19 14:51:34 +0300

    Add base implementation of json file log type parser

---
+++ src/Service/LogsParser/LogTypeParser/JsonFileLogTypeParser.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service\LogsParser\LogTypeParser;
+
+use App\Service\LogsParser\ParsedLog;
+
+class JsonFileLogTypeParser implements LogTypeParserInterface
+{
+
+    public static function getId(): string
+    {
+        return 'json';
+    }
+
+    public function parse(array $event): ?ParsedLog
+    {
+        $data = $event;
+
+        // TODO: this data cannot be extracted from logs so we must find other way to get them, now hard-coding
+        $masterUserId = 'id_of_master_user';
+        $resource = 'orders';
+        $url = 'https://pos-etail.wallmob.com';
+        $method = 'PATCH';
+
+        return new ParsedLog(json_encode($data), $method, "$url/{$resource}/{$data['id']}", $data['id'], $masterUserId);
+    }
+}
